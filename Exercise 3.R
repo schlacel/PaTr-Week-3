4f8afac2c5f1266f8d64c9bc8e6c427b3ca41dee
Celine Schlatter <celine.a.schlatter@bluewin.ch>
2021-05-07 11:11:48 +0200

    Task 2 completed

--- Exercise 3.R
+++ Exercise 3.R
@@ -10,6 +10,9 @@ library(lubridate)    # To handle dates and times
 
 caro <-read_delim("caro60.csv",",")
 
+#Measure the distance from every point to every other point within the temporal
+#window v
+
 caro <- caro %>%
   mutate(
     nMinus3 = sqrt((lag(E,3)-E)^2+(lag(N,3)-N)^2),   # distance to pos -3 minutes
@@ -19,6 +22,34 @@ caro <- caro %>%
     nPlus2  = sqrt((E-lead(E,2))^2+(N-lead(N,2))^2),  # distance to pos +2 minutes
     nPlus3  = sqrt((E-lead(E,3))^2+(N-lead(N,3))^2)  # distance to pos +3 minutes
   )
+
+View(caro)
+
+####Task 2 - Specify and apply threshold d #####################################
+
+#calculate the mean distance of nMinus2, nMinus1, nPlus1, nPlus2 for each row
+
+caro <- caro %>%
+  rowwise() %>%
+  mutate(
+    stepMean = mean(c(nMinus3, nMinus2, nMinus1,nPlus1,nPlus2,nPlus3))
+  ) %>%
+  ungroup() 
+
+#exploring Values of stepMean to choose the treshold 
+ggplot(data=caro, mapping = aes(x=stepMean))+
+  geom_histogram(binwidth = 1)
+
+ggplot(data=caro, mapping = aes(y=stepMean))+
+  geom_boxplot()
+
+summary(caro$stepMean)
+
+#we use the mean of all stepMean values =  6.951
+
+caro <- caro %>% 
+  ungroup() %>%
+  mutate(static = stepMean < mean(stepMean, na.rm = TRUE))
 
 View(caro)
 
